bfde0799286b3ea31d80d3c117eba559ebcb8637
Jeremy Cerise <jcerise06@gmail.com>
2013-08-26 11:48:53 -0600

    Added test for projects

--- test/models/ModelsTest.java
+++ test/models/ModelsTest.java
@@ -2,6 +2,7 @@ package models;
 
 import models.*;
 import org.junit.*;
+import java.util.List;
 import static org.junit.Assert.*;
 import play.test.WithApplication;
 import static play.test.Helpers.*;
@@ -26,5 +27,18 @@ public class ModelsTest extends WithApplication{
         assertNotNull(User.authenticate("jeremy@readmythings.com", "secret"));
         assertNull(User.authenticate("jeremy@readmythings.com", "badpassword"));
         assertNull(User.authenticate("fake@fake.com", "secret"));
+    }
+
+    @Test
+    public void findProjectsInvolving() {
+        new User("bob@gmail.com", "Bob", "secret").save();
+        new User("jane@gmail.com", "Jane", "secret").save();
+
+        Project.create("Play 2", "play", "bob@gmail.com");
+        Project.create("Play 1", "play", "jane@gmail.com");
+
+        List<Project> results = Project.findInvolving("bob@gmail.com");
+        assertEquals(1, results.size());
+        assertEquals("Play 2", results.get(0).name);
     }
 }
